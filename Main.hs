63347cd37f78c9b007d7d646389967b25ba17f2b
Jason Dagit <dagitj@gmail.com>
2011-04-02 23:49:19 -0700

    Doing a quick checkin because it finally works. More clean up needed.

--- Main.hs
+++ Main.hs
@@ -2,10 +2,15 @@ module Main where
 
 import Control.Monad
 import System.Environment
-import Graphics.Rendering.FreeType.Internal
+import Graphics.Rendering.FreeType.Internal as FT
 
 import Foreign
 import Foreign.Marshal
+import Foreign.C.String
+
+import System.Exit
+
+import Data.Array.IO as A
 
 main :: IO ()
 main = do
@@ -25,17 +30,79 @@ main = do
         , y = (height - 200) * 64
         })
   (filename:text:_) <- getArgs
-  Right library <- ft_Init_FreeType
-  Right face    <- ft_New_Face library filename 0
-  ft_Set_Char_Size face (50*64) 0 100 0
+
+  libraryptr <- malloc
+  library <- do
+    print $ libraryptr == nullPtr
+    ft_Init_FreeType libraryptr >>= print
+    peek libraryptr
+
+  faceptr <- malloc
+  print $ faceptr == nullPtr
+  face <- withCString filename $ \str -> do
+    print =<< ft_New_Face library str 0 faceptr
+    peek faceptr
+
+  image <- A.newArray
+    ((0,0), (fromIntegral height - 1, fromIntegral width - 1)) 0
+    :: IO (IOUArray (Int, Int) Int)
+
+  print =<< ft_Set_Char_Size face (50*64) 0 100 0
   forM_ text $ \c -> do
     withForeignPtr matrix $ \mp ->
       withForeignPtr pen $ \pp -> do
         ft_Set_Transform face mp pp
-        ft_Load_Char face (fromIntegral . fromEnum $ c) ft_LOAD_RENDER
-    -- drawBitmap
+        slot <- glyph face
+        print =<< ft_Load_Char face
+                               (fromIntegral . fromEnum $ c)
+                               ft_LOAD_RENDER
+        numFaces <- peek $ face_num_faces face
+        putStrLn $ "face->num_faces = " ++ show numFaces
+        v <- peek $ advance slot
+        putStrLn "advance: "
+        print v
+        numGlyphs <- peek $ face_num_glyphs face
+        putStrLn $ "numGlyphs = " ++ show numGlyphs
+        pen' <- peek pp
+        poke pp $ FT_Vector { x = x v + x pen'
+                            , y = y v + y pen' }
+        -- print =<< peek pp
+        b <- peek $ bitmap slot
+        left <- peek $ bitmap_left slot
+        top  <- peek $ bitmap_top slot
+        drawBitmap b
+                   image
+                   left
+                   (fromIntegral height - top)
+  showImage image
+  print =<< ft_Done_Face face
+  print =<< ft_Done_FreeType library
 
 
+drawBitmap :: FT_Bitmap -> IOUArray (Int, Int) Int
+           -> FT_Int -> FT_Int -> IO ()
+drawBitmap bitmap image x y = do
+  let xMax = x + width bitmap
+      yMax = y + rows bitmap
+  forM_ (zip [ x .. xMax - 1] [0 .. ]) $ \(i,p) ->
+    forM_ (zip [ y .. yMax - 1] [0 .. ]) $ \(j,q) -> do
+      let index = q * width bitmap + p
+      v <- readArray image (fromIntegral j, fromIntegral i) :: IO Int
+      b <- peek $ (buffer bitmap) `plusPtr` fromIntegral index
+      writeArray image (fromIntegral j, fromIntegral i) $ v .|. b
 
-  
-  
+showImage :: IOUArray (Int, Int) Int -> IO ()
+showImage image = do
+  ((hmin,wmin), (hmax,wmax)) <- getBounds image
+  forM_ [ hmin .. hmax ] $ \i -> do
+    forM_ [ wmin .. wmax ] $ \j -> do
+      v <- readArray image (i,j)
+      putc v
+    putChar '\n'
+  where
+  putc :: Int -> IO ()
+  putc c
+    | c == 0    = putChar '0'
+    | c < 128   = putChar '+'
+    | otherwise = putChar '*'
+
