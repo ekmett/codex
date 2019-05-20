1ce40bfe84b68d47d29e6f8e8e4eeb153286efd2
Edward Kmett <ekmett@gmail.com>
2019-05-20 11:35:30 -0400

    split out module for hkd, add default node count = width

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,24 +1,25 @@
 import Control.Monad.Primitive
 import Control.Monad.ST
 import Data.Functor.Identity
-import Data.Proxy
 import Test.Hspec
 
 import Codex.Packing
 -- import qualified Codex.UI as UI
 
-harnessM :: PrimMonad m => Int -> Int -> Int -> [Pt] -> m (Either [Maybe Pt] [Pt])
-harnessM w h n xs = do
-  ctx <- new w h n
-  pack ctx (\(Pt w' h') -> Box w' h' Proxy) (const _boxPosition) (const (runIdentity . _boxPosition)) xs
+harnessM :: PrimMonad m => Int -> Int -> [Pt] -> m (Either [Maybe Pt] [Pt])
+harnessM w h xs = do
+  ctx <- new_ w h
+  pack ctx boxy (\_ -> _boxPosition) (\_ -> runIdentity . _boxPosition) xs
 
-harness :: Int -> Int -> Int -> [Pt] -> Either [Maybe Pt] [Pt]
-harness w h n xs = runST $ harnessM w h n xs
+harness :: Int -> Int -> [Pt] -> Either [Maybe Pt] [Pt]
+harness w h xs = runST $ harnessM w h xs
 
 main :: IO ()
 main = hspec $ do
   describe "Codex.Packing" $ do
     it "can fill the packing region" $
-      harness 100 100 100 [Pt 50 50, Pt 50 50, Pt 50 50, Pt 50 50] `shouldBe` Right [Pt 0 0,Pt 50 0,Pt 0 50,Pt 50 50]
+      harness 100 100 [Pt 50 50, Pt 50 50, Pt 50 50, Pt 50 50] `shouldBe` Right [Pt 0 0,Pt 50 0,Pt 0 50,Pt 50 50]
     it "can run in IO" $
-      harnessM 100 100 100 [Pt 50 50, Pt 50 50, Pt 50 50, Pt 50 50] `shouldReturn` Right [Pt 0 0,Pt 50 0,Pt 0 50,Pt 50 50]
+      harnessM 100 100 [Pt 50 50, Pt 50 50, Pt 50 50, Pt 50 50] `shouldReturn` Right [Pt 0 0,Pt 50 0,Pt 0 50,Pt 50 50]
+    it "can be setup before you feed it data" $ 
+      (do ctx <- new_ 10 10; heuristic ctx BF) `shouldReturn` ()
