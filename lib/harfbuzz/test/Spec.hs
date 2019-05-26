b2d04408c449981b0c6d59c8249d27db1c0da6f3
Edward Kmett <ekmett@gmail.com>
2019-05-26 14:06:28 -0400

    start a tiny test suite

---
+++ lib/harfbuzz/test/Spec.hs
@@ -0,0 +1,43 @@
+{-# language OverloadedStrings #-}
+
+import Data.Const.ByteString
+import Data.Default
+import Graphics.Harfbuzz
+import Test.Hspec
+import Test.Tasty
+import Test.Tasty.Hspec
+
+spec :: Spec
+spec =
+  describe "hb_blob_t" $ do
+    it "should not construct the empty blob by default" $ do
+      blob_create "hello" MEMORY_MODE_READONLY `shouldNotReturn` def
+    it "has length" $ do
+      (blob_create "hello" MEMORY_MODE_READONLY >>= blob_get_length) `shouldReturn` 5
+    it "readonly is not immutable" $ do
+      (blob_create "hello" MEMORY_MODE_READONLY >>= blob_is_immutable) `shouldReturn` False
+    it "but make_immutable makes things immutable" $ do
+      let task = do
+           x <- blob_create "hello" MEMORY_MODE_READONLY
+           blob_make_immutable x
+           blob_is_immutable x
+      task `shouldReturn` True 
+    it "we get out what we put in" $ do
+      let task = do
+           x <- blob_create "hello" MEMORY_MODE_READONLY 
+           withBlobData x packACStringLen
+      task `shouldReturn` "hello"
+    it "trims correctly" $ do
+      let task = do
+           x <- blob_create "hello" MEMORY_MODE_WRITABLE
+           y <- blob_create_sub_blob x 2 2
+           withBlobData y packACStringLen
+      task `shouldReturn` "ll"
+
+main :: IO ()
+main = do
+  spec' <- testSpec "spec" spec
+  defaultMain $
+    testGroup "tests"
+      [ spec'
+      ]
