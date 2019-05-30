f86ae6864bce196021f787b53c5dff8c0ec20b8c
Sean Chalmers <sclhiannan@gmail.com>
2019-05-30 22:19:08 +1000

    Begin adding tests to fontconfig, improve default.nix

--- ui/ui.nix
+++ ui/ui.nix
@@ -1,7 +1,7 @@
 { mkDerivation, adjunctions, atlas, base, bytestring, const
 , containers, data-default, distributive, file-embed, fontconfig
 , freetype, gl, glow, harfbuzz, harfbuzz-icu, hkd, hspec
-, JuicyPixels, lens, linear, mtl, primitive, sdl2, StateVar, stdenv
+, lens, linear, mtl, primitive, sdl2, StateVar, stdenv
 , text, transformers, vector, weak
 }:
 mkDerivation {
@@ -14,14 +14,14 @@ mkDerivation {
   libraryHaskellDepends = [
     adjunctions atlas base bytestring const containers data-default
     distributive file-embed fontconfig freetype gl glow harfbuzz
-    harfbuzz-icu hkd JuicyPixels lens linear mtl primitive sdl2
+    harfbuzz-icu hkd lens linear mtl primitive sdl2
     StateVar text transformers vector weak
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
     adjunctions atlas base bytestring const containers data-default
     distributive file-embed fontconfig freetype gl glow harfbuzz
-    harfbuzz-icu hkd hspec JuicyPixels lens linear mtl primitive sdl2
+    harfbuzz-icu hkd hspec lens linear mtl primitive sdl2
     StateVar text transformers vector weak
   ];
   homepage = "https://github.com/ekmett/ui#readme";
