16460fb7d88520f4c0b6bc037b62132d348362be
Edward Kmett <ekmett@gmail.com>
2019-05-31 13:43:01 -0400

    finish harfbuzz-freetype

---
+++ harfbuzz-freetype/README.md
@@ -0,0 +1,20 @@
+harfbuzz-freetype
+=================
+
+[![Hackage](https://img.shields.io/hackage/v/harfbuzz-freetype.svg)](https://hackage.haskell.org/package/harfbuzz-freetype)
+
+Harfbuzz is a text shaping library that takes a sequence of unicode codepoints and a font, and selects a sequence of glyphs and placements for
+those glyphs to represent the desired text. 
+
+FreeType is a library that works with various font formats to extract glyph shapes and render images.
+
+This package provides additional interoperability between harfbuzz and FreeType 2.
+
+Contact Information
+-------------------
+
+Contributions and bug reports are welcome!
+
+Please feel free to contact me through github or on the #haskell IRC channel on irc.freenode.net.
+
+-Edward Kmett
--- harfbuzz-freetype/src/Graphics/Harfbuzz/FreeType.hs
+++ harfbuzz-freetype/src/Graphics/Harfbuzz/FreeType.hs
@@ -1,38 +1,61 @@
 {-# language TemplateHaskell #-}
 {-# language QuasiQuotes #-}
+{-# language ViewPatterns #-}
+{-# language OverloadedStrings #-}
 module Graphics.Harfbuzz.FreeType
-( face_create_referenced
-, font_create_referenced
-, font_get_face
-, font_load_flags -- statevar
-, font_changed
-, font_set_funcs
+( hb_ft_face_create_referenced
+, hb_ft_font_create_referenced
+, hb_ft_font_get_face
+, hb_ft_font_load_flags -- statevar
+, hb_ft_font_changed
+, hb_ft_font_set_funcs
 ) where
 
+import Control.Monad.IO.Class (MonadIO(..))
+import qualified Data.Map as Map
+import Data.StateVar (StateVar(..))
+import qualified Foreign.Concurrent as Concurrent
+import Foreign.Ptr (Ptr, nullPtr)
+import qualified Graphics.FreeType.Internal as FT
+import qualified Graphics.Harfbuzz.Internal as HB
 import qualified Language.C.Inline as C
-
-import Graphics.FreeType.Types as FreeType
-import Graphics.Harfbuzz.Internal as Harfbuzz
+import qualified Language.C.Inline.Context as C
+import qualified Language.C.Types as C
 
 -- TODO freetypeCtx
-let anti hsTyQ w = 
- in C.context $ C.baseCtx <> harfbuzzCtx <> mempty
+C.context $ C.baseCtx <> C.fptrCtx <> HB.harfbuzzCtx <> mempty
   { C.ctxTypesTable = Map.fromList
-    [ (C.TypeName "FT_Face", [t|Ptr FreeType.Face|])
+    [ (C.TypeName "FT_Face", [t|Ptr FT.FaceRec|])
     ]
-  , C.ctxAntiQuoters = Map.fromList
-    [ ("ftface", C.SomeAntiQuoter C.AntiQuoter
-        { C.aqParser = C.parseIdentifier <&> \hId -> (C.mangleHaskellIdentifier hId, C.TypeSpecifier mempty $ C.TypeName "FT_Face", hId)
-        , C.aqMarshaller = \_ _ _ cId -> (,) <$> [t|Ptr FreeType.Face|] <*> [|withSelf (coerce $(getHsVariable "Graphics.Harfbuzz.FreeType" cId))|]
-      })
-    ]
-  } where
+  }
 
-face_create_referenced :: MonadIO m => FreeType.Face -> m Harfbuzz.Face
-face_create_referenced ftface = liftIO $ [C.exp|hb_face_t * { hb_ft_face_create_referenced($ftface:ftface) }|] >>= Harfbuzz.foreignFace
+C.include "<hb-ft.h>"
+C.include "<ft2build.h>"
+C.verbatim "#include FT_FREETYPE_H"
 
-font_create_referenced :: MonadIO m => FreeType.Face -> m Harfbuzz.Font
-font_create_referenced ftface = liftIO $ [C.exp|hb_face_t * { hb_ft_face_create_referenced($ftface:ftface) }|] >>= Harfbuzz.foreignFace
+hb_ft_face_create_referenced :: MonadIO m => FT.Face -> m HB.Face
+hb_ft_face_create_referenced ftface = liftIO $ [C.exp|hb_face_t * { hb_ft_face_create_referenced($fptr-ptr:(FT_Face ftface)) }|] >>= HB.foreignFace
 
-font_get_face :: MonadIO m => Harfbuzz.Font -> m (Maybe FreeType.Face)
-font_get_face font = [C.exp|FT_Face { hb_ft_font_get_face($font:font)
+hb_ft_font_create_referenced :: MonadIO m => FT.Face -> m HB.Font
+hb_ft_font_create_referenced ftface = liftIO $ [C.exp|hb_font_t * { hb_ft_font_create_referenced($fptr-ptr:(FT_Face ftface)) }|] >>= HB.foreignFont
+
+hb_ft_font_get_face :: MonadIO m => HB.Font -> m (Maybe FT.Face)
+hb_ft_font_get_face font = liftIO $ do
+  p <- [C.block|FT_Face {
+    FT_Face face = hb_ft_font_get_face($font:font);
+    if (face) FT_Reference_Face(face);
+    return face;
+  }|]
+  if p == nullPtr then return Nothing
+  else Just . FT.Face <$> Concurrent.newForeignPtr p [C.block|void { FT_Done_Face($(FT_Face p)); }|] -- TODO: FT.foreignFace
+
+hb_ft_font_load_flags :: HB.Font -> StateVar Int
+hb_ft_font_load_flags font = StateVar g s where
+  g = fromIntegral <$> [C.exp|int { hb_ft_font_get_load_flags($font:font) }|]
+  s (fromIntegral -> v) = [C.block|void { hb_ft_font_set_load_flags($font:font,$(int v)); }|]
+
+hb_ft_font_changed :: MonadIO m => HB.Font -> m ()
+hb_ft_font_changed font = liftIO [C.block|void { hb_ft_font_changed($font:font); }|]
+
+hb_ft_font_set_funcs :: MonadIO m => HB.Font -> m ()
+hb_ft_font_set_funcs font = liftIO [C.block|void { hb_ft_font_set_funcs($font:font); }|]
