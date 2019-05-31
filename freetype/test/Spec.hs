93d63672fd53b9d336e7966c2bdf43f4fe1cfbd0
Edward Kmett <ekmett@gmail.com>
2019-05-31 17:50:33 -0400

    start a test suite

---
+++ freetype/test/Spec.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Foreign.ForeignPtr.Unsafe
+import Foreign.Ptr
+import System.Mem (performMajorGC)
+import Test.Hspec as Hspec
+import Test.Tasty
+import Test.Tasty.Hspec
+
+import Graphics.FreeType.Internal
+import Graphics.FreeType.Library
+
+spec :: Spec
+spec = Hspec.after_ performMajorGC $ do
+  describe "Library" $ do
+    it "can be constructed from scratch" $ do
+      Library lib <- new_library
+      unsafeForeignPtrToPtr lib `shouldNotBe`  nullPtr
+    it "can be constructed with defaults" $ do
+      Library lib <- init_library
+      unsafeForeignPtrToPtr lib `shouldNotBe`  nullPtr
+
+main :: IO ()
+main = do
+  spec' <- testSpec "spec" spec
+  defaultMain $ 
+    testGroup "tests"
+      [ spec'
+      ]
