2a269a1e27c4c42f94a5fe57da6f90f396694c84
Edward Kmett <ekmett@gmail.com>
2019-06-01 18:30:42 -0400

    I can eat glass and this does not hurt me.

--- bidi-icu/test/Spec.hs
+++ bidi-icu/test/Spec.hs
@@ -4,13 +4,12 @@ module Main
 ( main
 ) where
 
--- import Data.Default
+import GHC.Exts
 import Data.Text.ICU.Bidi
 import System.Mem
 import Test.Hspec as Hspec
 import Test.Tasty
 import Test.Tasty.Hspec
-
 
 spec :: Spec
 spec = Hspec.after_ performMajorGC $ do
@@ -25,6 +24,14 @@ spec = Hspec.after_ performMajorGC $ do
       getLogicalMap x `shouldReturn` [0..4]
       getVisualMap x `shouldReturn` [0..4]
       getLevels x `shouldReturn` [Level 0,Level 0,Level 0,Level 0,Level 0]
+    it "can handle Arabic" $ do
+      x <- open
+      getBaseDirection "أنا قادر على أكل الزجاج و هذا لا يؤلمني." `shouldBe` RTL
+      setPara x "This is an Arabic sentence: أنا قادر على أكل الزجاج و هذا لا يؤلمني. It is surrounded by English" DEFAULT_LTR Nothing
+      getLength x `shouldReturn` 96
+      getLogicalMap x `shouldReturn` fromList ([0..27] ++ [66,65..28] ++ [67..95])
+      getVisualMap x `shouldReturn` fromList ([0..27] ++ [66,65..28] ++ [67..95])
+      getLevels x `shouldReturn` fromList (replicate 28 (Level 0) ++ replicate 39 (Level 1) ++ replicate 29 (Level 0))
 
 main :: IO ()
 main = do
