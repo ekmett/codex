e21f5f2fd863d41d939b822b2f965e33a7d0bb85
Edward Kmett <ekmett@gmail.com>
2019-06-05 01:09:00 -0400

    Vector Feature is way more cacheable than [Feature]

--- harfbuzz/src/Graphics/Harfbuzz/Shape.hs
+++ harfbuzz/src/Graphics/Harfbuzz/Shape.hs
@@ -29,6 +29,8 @@ module Graphics.Harfbuzz.Shape
 
 import Control.Monad.Primitive
 import Data.Functor ((<&>))
+import Data.Int
+import Data.Vector.Storable
 import Foreign.C.Types
 import Foreign.Marshal.Array
 import Foreign.Ptr
@@ -37,73 +39,66 @@ import qualified Language.C.Inline as C
 import Graphics.Harfbuzz.Internal
 import Graphics.Harfbuzz.Private
 
-C.context $ C.baseCtx <> harfbuzzCtx
+C.context $ C.baseCtx <> C.vecCtx <> harfbuzzCtx
 C.include "<hb.h>"
 
-shape :: PrimMonad m => Font (PrimState m) -> Buffer (PrimState m) -> [Feature] -> m ()
+shape :: PrimMonad m => Font (PrimState m) -> Buffer (PrimState m) -> Vector Feature -> m ()
 shape font buffer features = unsafeIOToPrim $
-  withArrayLen features $ \ (fromIntegral -> len) pfeatures ->
+  [C.block|void{
+    hb_shape($font:font,$buffer:buffer,$vec-ptr:(const hb_feature_t * features),$vec-len:features);
+  }|]
+
+shape_full :: PrimMonad m => Font (PrimState m) -> Buffer (PrimState m) -> Vector Feature -> [Shaper] -> m ()
+shape_full font buffer features shapers = unsafeIOToPrim $
+  withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
     [C.block|void{
-      hb_shape($font:font,$buffer:buffer,$(const hb_feature_t * pfeatures),$(unsigned int len));
+      hb_shape_full($font:font,$buffer:buffer,$vec-ptr:(const hb_feature_t * features),$vec-len:features,$(const char * const * pshapers));
     }|]
 
-shape_full :: PrimMonad m => Font (PrimState m) -> Buffer (PrimState m) -> [Feature] -> [Shaper] -> m ()
-shape_full font buffer features shapers = unsafeIOToPrim $
-  withArrayLen features $ \ (fromIntegral -> len) pfeatures ->
-    withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
-      [C.block|void{
-         hb_shape_full($font:font,$buffer:buffer,$(const hb_feature_t * pfeatures),$(unsigned int len),$(const char * const * pshapers));
-      }|]
-
-shape_plan_create :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> [Feature] -> [Shaper] -> m (ShapePlan (PrimState m))
+shape_plan_create :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> Vector Feature -> [Shaper] -> m (ShapePlan (PrimState m))
 shape_plan_create face props features shapers = unsafeIOToPrim $
-  withArrayLen features $ \ (fromIntegral -> len) pfeatures ->
-    withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
-      [C.exp|hb_shape_plan_t * {
-         hb_shape_plan_create($face:face,$segment-properties:props,$(const hb_feature_t * pfeatures),$(unsigned int len),$(const char * const * pshapers))
-      }|] >>= foreignShapePlan
+  withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
+    [C.exp|hb_shape_plan_t * {
+      hb_shape_plan_create($face:face,$segment-properties:props,$vec-ptr:(const hb_feature_t * features),$vec-len:features,$(const char * const * pshapers))
+    }|] >>= foreignShapePlan
 
-shape_plan_create_cached :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> [Feature] -> [Shaper] -> m (ShapePlan (PrimState m))
+shape_plan_create_cached :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> Vector Feature -> [Shaper] -> m (ShapePlan (PrimState m))
 shape_plan_create_cached face props features shapers = unsafeIOToPrim $
-  withArrayLen features $ \ (fromIntegral -> len) pfeatures ->
-    withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
-      [C.exp|hb_shape_plan_t * {
-         hb_shape_plan_create_cached($face:face,$segment-properties:props,$(const hb_feature_t * pfeatures),$(unsigned int len),$(const char * const * pshapers))
-      }|] >>= foreignShapePlan
+  withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
+    [C.exp|hb_shape_plan_t * {
+      hb_shape_plan_create_cached($face:face,$segment-properties:props,$vec-ptr:(const hb_feature_t * features),$vec-len:features,$(const char * const * pshapers))
+    }|] >>= foreignShapePlan
 
-shape_plan_create2 :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> [Feature] -> [Int] -> [Shaper] -> m (ShapePlan (PrimState m))
+shape_plan_create2 :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> Vector Feature -> Vector Int32 -> [Shaper] -> m (ShapePlan (PrimState m))
 shape_plan_create2 face props features coords shapers = unsafeIOToPrim $
-  withArrayLen features $ \ (fromIntegral -> len) pfeatures ->
-    withArrayLen (fromIntegral <$> coords) $ \ (fromIntegral -> num_coords) pcoords ->
-      withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
-        [C.exp|hb_shape_plan_t * {
-           hb_shape_plan_create2(
-             $face:face,
-             $segment-properties:props,
-             $(const hb_feature_t * pfeatures),$(unsigned int len),
-             $(const int * pcoords),$(unsigned int num_coords),
-             $(const char * const * pshapers))
-        }|] >>= foreignShapePlan
+  withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
+    [C.exp|hb_shape_plan_t * {
+      hb_shape_plan_create2(
+        $face:face,
+        $segment-properties:props,
+        $vec-ptr:(const hb_feature_t * features),$vec-len:features,
+        $vec-ptr:(const int32_t * coords),$vec-len:coords,
+        $(const char * const * pshapers)
+      )
+    }|] >>= foreignShapePlan
 
-shape_plan_create_cached2 :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> [Feature] -> [Int] -> [Shaper] -> m (ShapePlan (PrimState m))
+shape_plan_create_cached2 :: PrimMonad m => Face (PrimState m) -> SegmentProperties -> Vector Feature -> Vector Int32 -> [Shaper] -> m (ShapePlan (PrimState m))
 shape_plan_create_cached2 face props features coords shapers = unsafeIOToPrim $
-  withArrayLen features $ \ (fromIntegral -> len) pfeatures ->
-    withArrayLen (fromIntegral <$> coords) $ \ (fromIntegral -> num_coords) pcoords ->
-      withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
-        [C.exp|hb_shape_plan_t * {
-           hb_shape_plan_create_cached2(
-             $face:face,
-             $segment-properties:props,
-             $(const hb_feature_t * pfeatures),$(unsigned int len),
-             $(const int * pcoords),$(unsigned int num_coords),
-             $(const char * const * pshapers))
-        }|] >>= foreignShapePlan
+  withArray0 SHAPER_INVALID shapers $ \ (castPtr -> pshapers) ->
+    [C.exp|hb_shape_plan_t * {
+      hb_shape_plan_create_cached2(
+        $face:face,
+        $segment-properties:props,
+        $vec-ptr:(const hb_feature_t * features),$vec-len:features,
+        $vec-ptr:(const int32_t * coords),$vec-len:coords,
+        $(const char * const * pshapers)
+      )
+    }|] >>= foreignShapePlan
 
-shape_plan_execute :: PrimMonad m => ShapePlan (PrimState m) -> Font (PrimState m) -> Buffer (PrimState m) -> [Feature] -> m Bool
+shape_plan_execute :: PrimMonad m => ShapePlan (PrimState m) -> Font (PrimState m) -> Buffer (PrimState m) -> Vector Feature -> m Bool
 shape_plan_execute plan font buffer features = unsafeIOToPrim $
-  withArrayLen features $ \ (fromIntegral -> len) pfeatures ->
     [C.exp|hb_bool_t {
-      hb_shape_plan_execute($shape-plan:plan,$font:font,$buffer:buffer,$(const hb_feature_t * pfeatures),$(unsigned int len))
+      hb_shape_plan_execute($shape-plan:plan,$font:font,$buffer:buffer,$vec-ptr:(const hb_feature_t * features),$vec-len:features)
     }|] <&> cbool
 
 shape_plan_get_shaper :: PrimMonad m => ShapePlan (PrimState m) -> m Shaper
