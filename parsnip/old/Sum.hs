bef07b76670e20ea37c3d2e75e3770dc94515b65
Edward Kmett <ekmett@gmail.com>
2019-06-11 00:46:59 -0400

    use null-terminated inputs?

--- parsnip/old/Sum.hs
+++ parsnip/old/Sum.hs
@@ -21,8 +21,11 @@
 
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Primitive
 import Control.Monad.ST
-import Data.ByteString.Internal as B
+import qualified Data.ByteString as B
+import Data.ByteString.Internal (ByteString(..))
+import qualified Data.ByteString.Internal as B
 import Data.Primitive.ByteArray
 import Data.Kind
 import Data.String
@@ -88,9 +91,6 @@ instance Functor (Parser s) where
     Fail q t -> Fail q t
   {-# inline (<$) #-}
 
---adjust :: Addr# -> Addr# -> Int# -> Int#
---adjust p q i = minusAddr# p q +# i -- note this is adding a negative
-
 instance Applicative (Parser s) where
   pure a = Parser $ \ p s -> OK a p s
   {-# inline pure #-}
@@ -131,37 +131,69 @@ instance MonadPlus (Parser s) where
   mzero = empty
   {-# inline mzero #-}
 
--- foreign import ccall unsafe "string.h memcmp" c_memcmp :: Ptr Word8 -> Ptr Word8 -> CSize -> IO CInt
-foreign import ccall unsafe "string.h memcmp" c_memcmp :: Addr# -> Addr# -> CSize -> IO CInt -- State# s -> (# State# s, CInt #)
+foreign import ccall unsafe "string.h strncmp" c_strncmp :: Addr# -> Addr# -> CSize -> IO CInt
+foreign import ccall unsafe "string.h strlen" c_strlen :: Addr# -> IO CSize
+foreign import ccall unsafe "string.h strlen" pure_strlen :: Addr# -> CSize
 
+cint :: CInt -> Int#
+cint (fromIntegral -> I# i) = i
+
+csize :: CSize -> Int#
+csize (fromIntegral -> I# i) = i
+
+-- super unsafe
+litN :: Addr# -> CSize -> Parser s ByteString
+litN q n = Parser $ \p s -> case unsafeCoerce# (internal $ c_strncmp p q n) s of
+    (# t, 0 #) -> OK (unsafeLiteralByteStringN q n) (p `plusAddr#` csize n) s
+    (# t, _ #) -> Fail p t
+
+lit :: Addr# -> Parser s ByteString
+lit q = litN q (pure_strlen q)
+
+literalForeignPtrContents :: ForeignPtrContents 
+literalForeignPtrContents = unsafeDupablePerformIO $ primitive $ \s -> case newByteArray# 0# s of
+  (# t, a #) -> (# t, PlainPtr a #)
+-- {-# noinline literalForeignPtrContents #-}
+
+unsafeLiteralForeignPtr :: Addr# -> ForeignPtr Word8
+unsafeLiteralForeignPtr addr = ForeignPtr addr undefined -- literalForeignPtrContents
+
+unsafeLiteralByteStringN :: Addr# -> CSize -> ByteString
+unsafeLiteralByteStringN p n = PS (unsafeLiteralForeignPtr p) 0 (fromIntegral n)
+
+unsafeLiteralByteString :: Addr# -> ByteString
+unsafeLiteralByteString p = unsafeLiteralByteStringN p (pure_strlen p)
+
+{-
 rest :: forall s. ReifiesBase s => Parser s ByteString
 rest = Parser $ \p -> OK (PS (ForeignPtr p guts) 0 (I# (minusAddr# end p))) p where
   Base (ForeignPtr q guts) end = reflectBase @s
+-}
 
-
-{-
 -- perhaps this interface is a little low level. hrmm
 instance a ~ ByteString => IsString (Parser s a) where
   fromString "" = pure B.empty
-  fromString xs = Parser $ \i p s -> case sizeOfByteArray# ba of
-      n -> if isTrue# (i <# n) then Fail p s
-        else case c_memcmp (byteArrayContents# ba) p (fromIntegral (I# n)) of 
-           IO f -> case f s of
-             (# t, i #)
-               | i /= 0 -> Fail p t
-               | otherwise -> OK bs (p `plusAddr#` n) s
-    where ba = pinnedPrimArrayFromString xs
-          bs = B.PS (ForeignPtr (byteArrayContents# ba) (PlainPtr ba)) 0 (I# (sizeOfByteArray# ba)))
--}
- 
-pinnedByteArrayFromString :: String -> ByteArray
-pinnedByteArrayFromString xs = byteArrayFromListN (length xs) xs
+  fromString xs = Parser $ \p s -> case sizeofMutableByteArray# ba of
+    n -> case unsafeCoerce# (internal (c_strncmp (mutableByteArrayContents# ba) p (fromIntegral $ I# n))) s of
+      (# t, i #)
+        | i /= 0    -> Fail p t
+        | otherwise -> OK bs (plusAddr# p n) t
+    where MutableByteArray ba = pinnedByteArrayFromString0 xs
+          bs = B.PS (ForeignPtr (mutableByteArrayContents# ba) (PlainPtr ba)) 0 (I# (sizeofMutableByteArray# ba))
 
-pinnedByteArrayFromStringN :: Int -> String -> ByteArray
-pinnedByteArrayFromStringN n ys = runST $ do
-  marr <- newPinnedByteArray n
+-- !(*@#U missing primitive
+mutableByteArrayContents# :: MutableByteArray# s -> Addr# 
+mutableByteArrayContents# arr = byteArrayContents# (unsafeCoerce# arr)
+
+-- hacked to return in any region
+pinnedByteArrayFromString0 :: String -> MutableByteArray RealWorld
+pinnedByteArrayFromString0 xs = pinnedByteArrayFromStringN0 (length xs) xs
+
+pinnedByteArrayFromStringN0 :: Int -> String -> MutableByteArray RealWorld
+pinnedByteArrayFromStringN0 n ys = runST $ unsafeSTToPrim $ do
+  marr <- newPinnedByteArray (n+1)
   let go !ix [] = if ix == n
-        then return ()
+        then writeByteArray marr ix (0 :: Word8)
         else fail "pinnedByteArrayFromStringN: list length less than specified size"
       go !ix (x : xs) = if ix < n
         then do
@@ -169,7 +201,8 @@ pinnedByteArrayFromStringN n ys = runST $ do
           go (ix + 1) xs
         else fail "pinnedByteArrayFromStringN: list length greater than specified size"
   go 0 ys
-  unsafeFreezeByteArray marr
+  pure marr
+  --unsafeFreezeByteArray marr
 
 ---------------------------------------------------------------------------------------
 -- * Running parsers and positioning
@@ -180,15 +213,15 @@ deriving instance Show Location
 -- | Reifying the base position as a dictionary avoids passing it at all down branches
 -- that do not use it, letting GHC know it is a constant.
 
-data Base s = Base {-# unpack #-} !(ForeignPtr Word8) Addr#
+data Base s = Base {-# unpack #-} !(ByteString) Addr# Addr#
 
 class ReifiesBase (s :: Type) where
   reflectBase :: Base s
 
 -- extract current location
 pos :: forall s. ReifiesBase s => Parser s Int
-pos = Parser $ \ p s -> OK (I# (minusAddr# p b)) p s where
-  Base (ForeignPtr b _) _ = reflectBase @s
+pos = Parser $ \ p s -> OK (I# (minusAddr# p q)) p s where
+  Base _ q _ = reflectBase @s
 {-# inline pos #-}
 
 data Wrap s a = Wrap (ReifiesBase s => Proxy# s -> Parser s a)
@@ -197,8 +230,8 @@ withBase :: (ReifiesBase s => Proxy# s -> Parser s a)
          -> Base s -> Proxy# s -> Parser s a
 withBase f x y = magicDict (Wrap f) x y
 
-finish :: Addr# -> Addr# -> ByteString -> Option a -> Either Location a
-finish p q bs = \case
+finish :: ByteString -> Addr# -> Addr# -> Option a -> Either Location a
+finish bs p q = \case
   Some a -> Right a
   None -> Left (location bs (I# (minusAddr# p q)))
 
@@ -206,12 +239,12 @@ finish p q bs = \case
 parse :: (forall s. ReifiesBase s => Parser s a) -> ByteString -> Either Location a
 parse m bs@(PS fp0 o (I# len)) = case plusForeignPtr fp0 o of
   fp -> unsafeDupablePerformIO $
-    withForeignPtr fp $ \ (Ptr p) ->
-      IO $ \s -> case runParser (withBase (\_ -> m) (Base fp (p `plusAddr#` len)) proxy#) p s of
-        (# m, q, t #) -> (# t, finish p q bs m #)
+    B.useAsCString bs $ \(Ptr p) -> -- now it is null terminated
+      IO $ \s -> case runParser (withBase (\_ -> m) (Base bs p (plusAddr# p len)) proxy#) p s of
+        (# m, q, t #) -> (# t, finish bs p q m #)
 
 main :: IO ()
 main = do
   print $ parse pos ""
   print $ do parse empty "money" :: Either Location Int
-  print $ parse rest "yo" 
+  print $ parse (lit "yo"#) "yo"
