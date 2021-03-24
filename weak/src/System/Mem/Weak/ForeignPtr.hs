07809d1f8eb23d2028d58c83517c0bc933bf1458
Edward Kmett <ekmett@gmail.com>
2021-03-23 17:10:13 -0700

    weak

--- weak/src/System/Mem/Weak/ForeignPtr.hs
+++ weak/src/System/Mem/Weak/ForeignPtr.hs
@@ -1,5 +1,6 @@
+{-# Language CPP #-}
 -- |
--- Copyright :  (c) 2019 Edward Kmett
+-- Copyright :  (c) 2019-2021 Edward Kmett
 -- License   :  BSD-2-Clause OR Apache-2.0
 -- Maintainer:  Edward Kmett <ekmett@gmail.com>
 -- Stability :  experimental
@@ -15,19 +16,43 @@ import GHC.ForeignPtr
 import System.Mem.Weak
 import System.Mem.Weak.IORef
 
--- | Make a 'Weak' reference from a 'ForeignPtr'. This attaches to an IORef down inside of the ForeignPtr
--- that holds onto the finalizers. This is safe than attaching to the ForeignPtr itself.
+-- | Make a 'Weak' reference from a 'Foreign.ForeignPtr.ForeignPtr'. 
+-- This attaches to the 'Data.IORef.IORef' down inside of the 'Foreign.ForeignPtr.ForeignPtr' that
+-- holds onto the finalizers. This is safer than attaching to the 'Foreign.ForeignPtr.ForeignPtr'
+-- itself.
+-- 
+-- However: this does not work with 'GHC.ForeignPtr.PlainPtr' or 'GHC.ForeignPtr.FinalPtr'-based
+-- 'Foreign.ForeignPtr.ForeignPtr's, so make sure you know how you built your 
+-- 'Foreign.ForeignPtr.ForeignPtr'.
 mkWeakForeignPtr :: ForeignPtr k -> v -> Maybe (IO ()) -> IO (Weak v)
 mkWeakForeignPtr (ForeignPtr _ k) v fin = mkWeakIORef' (finalizers k) v fin where
   finalizers PlainPtr{} = error "mkWeakForeignPtr: PlainPtr encountered"
+#if MIN_VERSION_base(4,15,0)
+  finalizers FinalPtr = error "mkWeakForeignPtr: FinalPtr encountered"
+#endif
   finalizers (MallocPtr _ p) = p
   finalizers (PlainForeignPtr p) = p 
 {-# inlinable mkWeakForeignPtr #-}
 
+-- | Functions like 'System.Mem.Weak.mkWeakPtr' but for 'Foreign.ForeignPtr.ForeignPtr's.
+--
+-- A specialised version of 'mkWeakForeignPtr', where the key and the value are the same object:
+--
+-- @
+-- 'mkWeakForeignPtrPtr' key finalizer = 'mkWeakForeignPtr' key key finalizer
+-- @
 mkWeakForeignPtrPtr :: ForeignPtr a -> Maybe (IO ()) -> IO (Weak (ForeignPtr a))
 mkWeakForeignPtrPtr k = mkWeakForeignPtr k k
 {-# inlinable mkWeakForeignPtrPtr #-}
 
+-- | Functions like 'System.Mem.Weak.mkWeakPair' but for 'Foreign.ForeignPtr.ForeignPtr's.
+--
+-- A specialised version of 'mkWeakForeignPtr' where the value
+-- is actually a pair of the key and value passed to 'mkWeakForeignPtrPair':
+-- @
+-- 'mkWeakForeignPtrPair' key val finalizer ≡ 'mkWeakForeignPtr' key (key,val) finalizer
+-- @
+-- The advantage of this is that the key can be retrieved by 'deRefWeak' in addition to the value.
 mkWeakForeignPtrPair :: ForeignPtr k -> v -> Maybe (IO ()) -> IO (Weak (ForeignPtr k, v))
 mkWeakForeignPtrPair k v = mkWeakForeignPtr k (k,v)
 {-# inlinable mkWeakForeignPtrPair #-}
--- weak/src/System/Mem/Weak/IORef.hs
+++ weak/src/System/Mem/Weak/IORef.hs
@@ -2,7 +2,7 @@
 {-# language UnboxedTuples #-}
 {-# language LambdaCase #-}
 -- |
--- Copyright :  (c) 2019 Edward Kmett
+-- Copyright :  (c) 2019-2021 Edward Kmett
 -- License   :  BSD-2-Clause OR Apache-2.0
 -- Maintainer:  Edward Kmett <ekmett@gmail.com>
 -- Stability :  experimental
@@ -19,18 +19,35 @@ import GHC.Weak
 import GHC.IORef
 import GHC.STRef
 
--- | Make an arbitrary 'Weak' reference from a 'IORef'.
--- Provides a more general API than the crippled one offered by @Data.IORef@
+-- | Make an arbitrary 'Weak' reference from a 'Data.IORef.IORef'.
+--
+-- Provides a more general API than the crippled one offered by 'Data.IORef.mkWeakIORef'
+-- to match the power of 'mkWeak'.
 mkWeakIORef' :: IORef k -> v -> Maybe (IO ()) -> IO (Weak v)
 mkWeakIORef' (IORef (STRef k#)) v = \case
   Nothing             -> IO $ \s -> case mkWeakNoFinalizer# k# v s of (# s1, w #) -> (#s1, Weak w #)
   Just (IO finalizer) -> IO $ \s -> case mkWeak# k# v finalizer s of (# s1, w #) -> (#s1, Weak w #)
 {-# inlinable mkWeakIORef' #-}
 
+-- | Functions like 'System.Mem.Weak.mkWeakPtr' but for 'Data.IORef.IORef's.
+--
+-- Make an arbitrary 'Weak' reference from an 'Data.IORef.IORef' and an optional finalizer.
+--
+-- A specialised version of 'mkWeakIORef'' where the key and value are the same.
 mkWeakIORefPtr :: IORef k -> Maybe (IO ()) -> IO (Weak (IORef k))
 mkWeakIORefPtr k = mkWeakIORef' k k
 {-# inlinable mkWeakIORefPtr #-}
 
+-- | 
+--
+-- A specialised version of 'mkWeakIORef'' where the value is actually a pair of the
+-- key and value passed to 'mkWeakIORefPair':
+--
+-- @
+-- 'mkWeakIORefPair' key val finalizer ≡ 'mkWeakIORef'' key (key,val) finalizer
+-- @
+--
+-- The advantage of this is that the key can be retrieved by 'deRefWeak' in addition to the value.
 mkWeakIORefPair :: IORef k -> v -> Maybe (IO ()) -> IO (Weak (IORef k, v))
 mkWeakIORefPair k v = mkWeakIORef' k (k,v)
 {-# inlinable mkWeakIORefPair #-}
